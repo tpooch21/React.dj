33bc9b5ee4632e42e762163900bce4c974a469a6
Trevor Puccini <trevorpuccini21@gmail.com>
2020-06-06 14:32:11 -0700

    Testing with Web Audio API to produce sounds on screen

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -8,10 +8,35 @@ class App extends React.Component {
       running: true
     };
 
+    this.start = this.start.bind(this);
+    this.stop = this.stop.bind(this);
+  }
+
+  componentDidMount() {
+
+  }
+
+  start() {
+    debugger;
+    osc = context.createOscillator();
+    osc.connect(context.destination);
+    osc.frequency.value = 220;
+    osc.start();
+  }
+
+  stop() {
+    osc.stop();
+    osc.disconnect();
   }
 
   render() {
-    return <h1>Hello World</h1>
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <button onClick={this.start}>Play</button>
+        <button onClick={this.stop}>Stop</button>
+      </div>
+    );
   }
 
 };
