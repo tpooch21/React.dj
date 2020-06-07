a35bb127f2bebd90d5b17320502b00533313b764
Trevor Puccini <trevorpuccini21@gmail.com>
2020-06-06 20:48:53 -0700

    Styling changes to select menus

--- client/src/components/ScaleForm.jsx
+++ client/src/components/ScaleForm.jsx
@@ -3,27 +3,72 @@ import styled from 'styled-components';
 import $ from 'jquery';
 
 const ScaleDiv = styled.div`
-  width: 20%;
+  width: 40%;
   height: 100px;
   display: flex;
   flex-direction: row;
-  justify-content: space-around;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+const SelectLabel = styled.h4`
+  color: white;
+  font-family: Muli, sans-serif;
+  margin-right: 10px;
+`;
+
+const NoteWrapper = styled.div`
+  height: 75px;
+  display: flex;
+  width: 100px
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
 `;
 
 const NoteSelect = styled.select`
-  height: 25%;
+  height: 25px;
+  margin-right: 35px;
+`;
+
+const MajorWrapper = styled.div`
+  height: 75px;
+  display: flex;
+  width: 100px
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  position: relative;
 `;
 
 const MajorOrMinor = styled.select`
-  height: 25%;
+  height: 25px;
+  margin-right: 35px;
 `;
 
 const ScaleAndNoteOption = styled.option`
+  font-family: Muli, sans-serif;
+`;
 
+const ButtonWrapper = styled.div`
+  height: 75px;
+  display: flex;
+  width: 100px
+  flex-direction: row;
+  justify-content: flex-end;
+  align-items: center;
+  position: relative;
 `;
 
 const GetNotesButton = styled.button`
-  height: 25%;
+  height: 25px;
+  font-family: Muli, sans-serif;
+  color: white;
+  border-radius: 11px;
+  background: linear-gradient(145deg, #1e1e1e, #191919);
+  box-shadow:  5px 5px 10px #151515,
+             -5px -5px 10px #232323;
 `;
 
 class ScaleForm extends React.Component {
@@ -75,27 +120,33 @@ class ScaleForm extends React.Component {
   render() {
     return (
       <ScaleDiv>
-        <h5>Note: </h5>
-        <NoteSelect onChange={(e) => this.onNoteSelection(e)}>
-          <ScaleAndNoteOption value='A'>A</ScaleAndNoteOption>
-          <ScaleAndNoteOption value='Asharp'>A#</ScaleAndNoteOption>
-          <ScaleAndNoteOption value='B'>B</ScaleAndNoteOption>
-          <ScaleAndNoteOption value='C'>C</ScaleAndNoteOption>
-          <ScaleAndNoteOption value='Csharp'>C#</ScaleAndNoteOption>
-          <ScaleAndNoteOption value='D'>D</ScaleAndNoteOption>
-          <ScaleAndNoteOption value='Dsharp'>D#</ScaleAndNoteOption>
-          <ScaleAndNoteOption value='E'>E</ScaleAndNoteOption>
-          <ScaleAndNoteOption value='F'>F</ScaleAndNoteOption>
-          <ScaleAndNoteOption value='Fsharp'>F#</ScaleAndNoteOption>
-          <ScaleAndNoteOption value='G'>G</ScaleAndNoteOption>
-          <ScaleAndNoteOption value='Gsharp'>G#</ScaleAndNoteOption>
-        </NoteSelect>
-        <h5>Scale: </h5>
-        <MajorOrMinor onChange={(e) => this.onScaleSelection(e)}>
-          <ScaleAndNoteOption value="Major">Major</ScaleAndNoteOption>
-          <ScaleAndNoteOption value="Minor">Minor</ScaleAndNoteOption>
-        </MajorOrMinor>
-        <GetNotesButton onClick={this.getNotes}>Get Notes</GetNotesButton>
+        <NoteWrapper>
+          <SelectLabel>Note:</SelectLabel>
+          <NoteSelect onChange={(e) => this.onNoteSelection(e)}>
+            <ScaleAndNoteOption value='A'>A</ScaleAndNoteOption>
+            <ScaleAndNoteOption value='Asharp'>A#</ScaleAndNoteOption>
+            <ScaleAndNoteOption value='B'>B</ScaleAndNoteOption>
+            <ScaleAndNoteOption value='C'>C</ScaleAndNoteOption>
+            <ScaleAndNoteOption value='Csharp'>C#</ScaleAndNoteOption>
+            <ScaleAndNoteOption value='D'>D</ScaleAndNoteOption>
+            <ScaleAndNoteOption value='Dsharp'>D#</ScaleAndNoteOption>
+            <ScaleAndNoteOption value='E'>E</ScaleAndNoteOption>
+            <ScaleAndNoteOption value='F'>F</ScaleAndNoteOption>
+            <ScaleAndNoteOption value='Fsharp'>F#</ScaleAndNoteOption>
+            <ScaleAndNoteOption value='G'>G</ScaleAndNoteOption>
+            <ScaleAndNoteOption value='Gsharp'>G#</ScaleAndNoteOption>
+          </NoteSelect>
+        </NoteWrapper>
+        <MajorWrapper>
+          <SelectLabel>Scale:</SelectLabel>
+          <MajorOrMinor onChange={(e) => this.onScaleSelection(e)}>
+            <ScaleAndNoteOption value="Major">Major</ScaleAndNoteOption>
+            <ScaleAndNoteOption value="Minor">Minor</ScaleAndNoteOption>
+          </MajorOrMinor>
+        </MajorWrapper>
+        <ButtonWrapper>
+          <GetNotesButton onClick={this.getNotes}>Get Notes</GetNotesButton>
+        </ButtonWrapper>
       </ScaleDiv>
     );
 
