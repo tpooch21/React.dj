573757a0c8a63d5e7a8cbf7916b29395fa50e24d
Trevor Puccini <trevorpuccini21@gmail.com>
2020-06-07 19:39:07 -0700

    Add color changing click event to turn selected pad buttons orange

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import ScaleForm from './ScaleForm.jsx';
 import Instrument from './Instrument.jsx';
 import styled from 'styled-components';
-import {ControllerPlay} from '@styled-icons/entypo/ControllerPlay';
-import {Pause} from '@styled-icons/foundation/Pause';
 const Octavian = require('octavian');
 
 const MainDiv = styled.div`
@@ -20,54 +18,6 @@ const MainTitle = styled.h1`
   color: white;
   font-family: Muli, sans-serif;
   font-weight: bold;
-`;
-
-const PlayPauseWrapper = styled.div`
-  width: 15%;
-  height: 50px;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-;`
-
-const PlayWrapper = styled.div`
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-  background-color: #1c1c1c;
-  background: ${props => props.playing ? '#f28c26' : 'linear-gradient(145deg, #191919, #1e1e1e)'};
-  box-shadow:  6px 6px 12px #131313,
-             -6px -6px 12px #252525;
-  position: relative;
-  cursor: pointer;
-`;
-
-const PauseWrapper = styled.div`
-  width: 50px;
-  height: 50px;
-  border-radius: 50%;
-  background-color: #1c1c1c;
-  background: ${props => props.playing ? 'linear-gradient(145deg, #191919, #1e1e1e)' : '#f28c26'};
-  box-shadow:  6px 6px 12px #131313,
-             -6px -6px 12px #252525;
-  position: relative;
-  cursor: pointer;
-`;
-
-const ControllerIcon = styled(ControllerPlay)`
-  color: white;
-  height: 35px;
-  position: absolute;
-  top: 7px;
-  left: 9px;
-`;
-
-const PauseIcon = styled(Pause)`
-  color: white;
-  height: 35px;
-  position: absolute;
-  top: 7px;
-  left: 7px;
 `;
 
 class App extends React.Component {
@@ -95,7 +45,6 @@ class App extends React.Component {
     this.state.filledSpaces.push(this.state.firstOpenSpace);
     let newOpenSpace;
     for (let i = 0; i < 4; i++) {
-      debugger;
       if (this.state.filledSpaces.indexOf(i) === -1) {
         newOpenSpace = i;
         break;
@@ -133,14 +82,6 @@ class App extends React.Component {
       <MainDiv>
         <MainTitle>JAVASLAPZ</MainTitle>
         <ScaleForm selectNote={this.onNoteSelect}/>
-        <PlayPauseWrapper>
-          <PlayWrapper onClick={this.play} playing={this.state.playing}>
-            <ControllerIcon></ControllerIcon>
-          </PlayWrapper>
-          <PauseWrapper onClick={this.pause} playing={this.state.playing}>
-            <PauseIcon></PauseIcon>
-          </PauseWrapper>
-        </PlayPauseWrapper>
         <Instrument parentState={this.state} remove={this.removeNote}></Instrument>
       </MainDiv>
     );
--- client/src/components/Instrument.jsx
+++ client/src/components/Instrument.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import {ControllerPlay} from '@styled-icons/entypo/ControllerPlay';
+import {Pause} from '@styled-icons/foundation/Pause';
 const Octavian = require('octavian');
 
 const PadWrapper = styled.div`
@@ -9,7 +11,7 @@ const PadWrapper = styled.div`
   flex-direction: row;
   justify-content: space-around;
   align-items: center;
-  margin-top: 50px;
+  margin-top: 20px;
   margin-bottom: 50px;
 `;
 
@@ -70,10 +72,62 @@ const PadButton = styled.button`
   width: 65px;
   height: 65px;
   border-radius: 15px;
-  background: linear-gradient(145deg, #1e1e1e, #191919);
+  background: ${props => props.selected ? '#f28c26' : 'linear-gradient(145deg, #1e1e1e, #191919)'};
   box-shadow:  8px 8px 16px #101010,
             -8px -8px 16px #282828;
   cursor: pointer;
+`;
+
+const PlayPauseWrapper = styled.div`
+  width: 60px;
+  height: 375px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+`;
+
+const PlayWrapper = styled.div`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: #1c1c1c;
+  background: ${props => props.playing ? '#f28c26' : 'linear-gradient(145deg, #191919, #1e1e1e)'};
+  box-shadow:  6px 6px 12px #131313,
+             -6px -6px 12px #252525;
+  position: relative;
+  cursor: pointer;
+  margin-bottom: 20px;
+  margin-left: 15px;
+`;
+
+const PauseWrapper = styled.div`
+  width: 50px;
+  height: 50px;
+  border-radius: 50%;
+  background-color: #1c1c1c;
+  background: ${props => props.playing ? 'linear-gradient(145deg, #191919, #1e1e1e)' : '#f28c26'};
+  box-shadow:  6px 6px 12px #131313,
+             -6px -6px 12px #252525;
+  position: relative;
+  cursor: pointer;
+  margin-left: 15px;
+`;
+
+const ControllerIcon = styled(ControllerPlay)`
+  color: white;
+  height: 35px;
+  position: absolute;
+  top: 7px;
+  left: 9px;
+`;
+
+const PauseIcon = styled(Pause)`
+  color: white;
+  height: 35px;
+  position: absolute;
+  top: 7px;
+  left: 7px;
 `;
 
 class Instrument extends React.Component {
@@ -82,14 +136,16 @@ class Instrument extends React.Component {
 
     this.state = {
       noteSelections: this.props.parentState.currentNotes,
-      row1Oscillators: [],
-      row2Oscillators: [],
-      row3Oscillators: [],
-      row4Oscillators: []
+      rowOscillators: {},
+      row1Oscillators: {},
+      row2Oscillators: {},
+      row3Oscillators: {},
+      playing: false
     };
 
     this.createOscillator = this.createOscillator.bind(this);
     this.play = this.play.bind(this);
+    this.stop = this.stop.bind(this);
   }
 
   createOscillator(row, col) {
@@ -105,19 +161,51 @@ class Instrument extends React.Component {
 
       osc.connect(gainNode);
       // setTimeout(() => osc.stop(), 1000);
-      osc.start();
-      // this.state.row1Oscillators.push(osc);
+      var rowToChange;
+      if (row === 0) {
+        this.state.rowOscillators[col] = osc;
+        rowToChange = 'rowOscillators';
+      } else if (row === 1) {
+        this.state.row1Oscillators[col] = osc;
+        rowToChange = 'row1Oscillators';
+      } else if (row === 2) {
+        this.state.row2Oscillators[col] = osc;
+        rowToChange = 'row2Oscillators';
+      } else {
+        this.state.row3Oscillators[col] = osc;
+        rowToChange = 'row3Oscillators';
+      };
+
+      debugger;
+      this.setState({
+        [rowToChange]: this.state[rowToChange]
+      });
+
     }
   }
 
   play() {
     debugger;
-    for (var i = 0; i < this.state.row1Oscillators.length; i++) {
-      var oscillator = this.state.row1Oscillators[i];
-      setTimeout(() => {oscillator.stop()}, 1000);
-      oscillator.start()
+    for (var key in this.state.rowOscillators) {
+      var oscillator1 = this.state.rowOscillators[key];
+      var oscillator2 = this.state.row1Oscillators[key];
+      var oscillator3 = this.state.row2Oscillators[key];
+      var oscillator4 = this.state.row3Oscillators[key];
+      // setTimeout(() => {oscillator.stop()}, 1000);
+      oscillator1.start()
+      oscillator2.start()
+      oscillator3.start()
+      oscillator4.start()
     }
+    this.setState({
+      playing: true
+    });
+  }
 
+  stop() {
+    for (var i = 0; i < this.state.row1Oscillators.length; i++) {
+
+    }
   }
 
   render() {
@@ -135,25 +223,49 @@ class Instrument extends React.Component {
         <PadButtonsWrapper>
           <PadButtonRow>
             {[0, 1, 2, 3].map(num => {
-              return <PadButton onClick={() => this.createOscillator(0, num)}></PadButton>
+              var selected = false;
+              if (this.state.rowOscillators[num]) {
+                selected = true;
+              }
+              return <PadButton selected={selected} onClick={() => this.createOscillator(0, num)}></PadButton>
             })}
           </PadButtonRow>
           <PadButtonRow>
             {[0, 1, 2, 3].map(num => {
-                return <PadButton onClick={() => this.createOscillator(1, num)}></PadButton>
+              var selected = false;
+              if (this.state.row1Oscillators[num]) {
+                selected = true;
+              }
+              return <PadButton selected={selected}onClick={() => this.createOscillator(1, num)}></PadButton>
               })}
           </PadButtonRow>
           <PadButtonRow>
             {[0, 1, 2, 3].map(num => {
-                return <PadButton onClick={() => this.createOscillator(2, num)}></PadButton>
+              var selected = false;
+              if (this.state.row2Oscillators[num]) {
+                selected = true;
+              }
+              return <PadButton selected={selected} onClick={() => this.createOscillator(2, num)}></PadButton>
               })}
           </PadButtonRow>
           <PadButtonRow>
             {[0, 1, 2, 3].map(num => {
-                return <PadButton onClick={() => this.createOscillator(3, num)}></PadButton>
+              var selected = false;
+              if (this.state.row3Oscillators[num]) {
+                selected = true;
+              }
+              return <PadButton selected={selected} onClick={() => this.createOscillator(3, num)}></PadButton>
               })}
           </PadButtonRow>
         </PadButtonsWrapper>
+        <PlayPauseWrapper>
+          <PlayWrapper playing={this.state.playing} onClick={this.play}>
+            <ControllerIcon></ControllerIcon>
+          </PlayWrapper>
+          <PauseWrapper playing={this.state.playing} onClick={this.stop}>
+            <PauseIcon></PauseIcon>
+          </PauseWrapper>
+        </PlayPauseWrapper>
       </PadWrapper>
     );
   }
