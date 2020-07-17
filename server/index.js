f4d0712444b91fba37128155447dbe8bc781e178
Trevor Puccini <trevorpuccini21@gmail.com>
2020-07-17 12:15:59 -0700

    Create demo directory with demo gif

--- server/index.js
+++ server/index.js
@@ -2,14 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const morgan = require('morgan');
-const port = 3000;
+const port = 3001;
 const controllers = require('./controllers.js');
 
 app.use(morgan('tiny'));
 
 app.use(express.static(path.join(__dirname + '/../client/public/')));
 
+app.get('/scale/:name/notes', controllers.getNotes);
+
 app.listen(port, () => {console.log(`Server is listening at http://localhost:${port}`)});
 
-app.get('/scale/:name/notes', controllers.getNotes);
 
